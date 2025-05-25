8ea804a7a9dc278376f82387c0374eced5084ce6
Juan Estiben Sanchez Martinez <84460077+jesm17@users.noreply.github.com>
2025-05-25 16:12:53 -0500

    add ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.medicationreminder.app',
+  appName: 'medicationreminder',
+  webDir: 'www'
+};
+
+export default config;
